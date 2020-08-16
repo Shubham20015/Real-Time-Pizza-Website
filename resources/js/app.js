629d12a378bc4b6c2915570360f3d50725c914c6
Shubham <Shubham20015>
2020-08-16 17:45:06 +0530

    Add Orders Table & Admin panel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
-const { update } = require("../../app/models/menu");
-
 import axios from 'axios'
 import Noty from 'noty'
+import { initAdmin } from './admin'
 
 let addToCart = document.querySelectorAll('.add-to-cart');
 let cartCounter = document.querySelector('#cartCounter');
@@ -31,3 +30,12 @@ addToCart.forEach((btn)=>{
         updateCart(pizza);
     })
 })
+
+const alertMsg = document.querySelector('#success-alert');
+if(alertMsg){
+    setTimeout(() => {
+        alertMsg.remove();
+    },2000);
+}
+
+initAdmin();
